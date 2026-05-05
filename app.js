f5bcf60d7252093092e2a7c31e5689bbe64dc8de
priti singh <preetisinghps019@gmail.com>
2026-05-05 23:42:41 +0530

    Added user authentication modules and updated UI

--- app.js
+++ app.js
@@ -8,13 +8,24 @@ const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+
+//const session = require("express-session");
+//const flash = require("connect-flash");
+//const MongoStore = require("connect-mongo");
+//const passport = require("passport");
+//const LocalStrategy = require("passport-local");
+
+//const User = require("./models/user.js");
+
 const listingRoutes = require("./routes/listings.js");
 const reviewRoutes = require("./routes/reviews.js");
+//const userRoutes = require("./routes/users.js");
 
 const ExpressError = require("./utils/expresserror.js");
 
 
 const mongo_url = process.env.mongo_url || "mongodb://127.0.0.1:27017/wanderlust";
+const AUTH_ENABLED = process.env.AUTH_ENABLED === "true";
 
 async function main() {
     await mongoose.connect(mongo_url);
@@ -32,11 +43,64 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-// Root route
+
+if (AUTH_ENABLED) {
+    const session = require("express-session");
+    const flash = require("connect-flash");
+    const MongoStore = require("connect-mongo");
+    const passport = require("passport");
+    const LocalStrategy = require("passport-local");
+    const User = require("./models/user.js");
+    const userRoutes = require("./routes/users.js");
+
+    const store = MongoStore.create({
+        mongoUrl: mongo_url,
+        crypto: {
+            secret: process.env.SESSION_SECRET || "mysupersecretcode",
+        },
+        touchAfter: 24 * 3600,
+    });
+
+    const sessionOptions = {
+        store,
+        secret: process.env.SESSION_SECRET || "mysupersecretcode",
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly: true,
+        },
+    };
+
+    app.use(session(sessionOptions));
+    app.use(flash());
+    app.use(passport.initialize());
+    app.use(passport.session());
+    passport.use(new LocalStrategy(User.authenticate()));
+    passport.serializeUser(User.serializeUser());
+    passport.deserializeUser(User.deserializeUser());
+
+    app.use((req, res, next) => {
+        res.locals.success = req.flash("success");
+        res.locals.error = req.flash("error");
+        res.locals.currUser = req.user;
+        next();
+    });
+
+    app.use("/", userRoutes);
+} else {
+    app.use((req, res, next) => {
+        res.locals.success = [];
+        res.locals.error = [];
+        res.locals.currUser = null;
+        next();
+    });
+}
+
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
-
 app.use("/listings", listingRoutes);
 app.use("/listings/:id/reviews", reviewRoutes);
 
@@ -54,4 +118,67 @@ app.use((err, req, res, next) => {
 app.listen(8080, () => {
     console.log("Server is listening on port 8080");
 });
+// const store = MongoStore.create({
+//     mongoUrl: mongo_url,
+//     crypto: {
+//         secret: process.env.SESSION_SECRET || "mysupersecretcode",
+//     },
+//     touchAfter: 24 * 3600,
+// });
+
+// const sessionOptions = {
+//     store,
+//     secret: process.env.SESSION_SECRET || "mysupersecretcode",
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: {
+//         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+//         maxAge: 7 * 24 * 60 * 60 * 1000,
+//         httpOnly: true,
+//     },
+// };
+
+// app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "views"));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(methodOverride("_method"));
+// app.engine("ejs", ejsMate);
+// app.use(express.static(path.join(__dirname, "/public")));
+// app.use(session(sessionOptions));
+// app.use(flash());
+// app.use(passport.initialize());
+// app.use(passport.session());
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+// app.use((req, res, next) => {
+//     res.locals.success = req.flash("success");
+//     res.locals.error = req.flash("error");
+//     res.locals.currUser = req.user;
+//     next();
+// });
+// // Root route
+// app.get("/", (req, res) => {
+//     res.send("Hi, I am root");
+// });
+
+// app.use("/listings", listingRoutes);
+// app.use("/listings/:id/reviews", reviewRoutes);
+// app.use("/", userRoutes);
+
+// // Catch-all for undefined routes
+// app.all("*", (req, res, next) => {
+//     next(new ExpressError(404, "Page not found!"));
+// });
+
+// // Error handling middleware
+// app.use((err, req, res, next) => {
+//     const { statusCode = 500, message = "Something went wrong" } = err;
+//     res.status(statusCode).render("error.ejs", { message });
+// });
+
+// app.listen(8080, () => {
+//     console.log("Server is listening on port 8080");
+// });
 
